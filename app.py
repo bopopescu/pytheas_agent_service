0afd51a4f82208f5ad8760ec16c6e5998cba71c9
AvrahamLa <avrahamla@gmail.com>
2019-05-26 22:12:59 +0300

    return app

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from service import Service
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
 	return 'OK!'
+
+
+@app.route('/api/get_attractions_for_profile', methods=['GET'])
+def get_attractions_for_profile():
+    profile_id = int(request.args.get('ProfileId'))
+    city_id = int(request.args.get('CityId')) if request.args.get('CityId') is not None else None
+    agent_service = Service()
+    result_vector = agent_service.predict_trip_for_profile(profile_id, city_id)
+    return jsonify({'Results': result_vector})
+
+
+if __name__ == '__main__':
+	app.run()
