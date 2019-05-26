40af78ee66fd6bf14759370a6c09460a433dffdd
AvrahamLa <avrahamla@gmail.com>
2019-05-26 23:47:09 +0300

    Update flask_service.py

--- app/flask_service.py
+++ app/flask_service.py
@@ -10,11 +10,12 @@ def index():
 
 @app.route('/api/get_attractions_for_profile', methods=['GET'])
 def get_attractions_for_profile():
-    profile_id = int(request.args.get('ProfileId'))
-    city_id = int(request.args.get('CityId')) if request.args.get('CityId') is not None else None
-    agent_service = Service()
-    result_vector = agent_service.predict_trip_for_profile(profile_id, city_id)
-    return jsonify({'Results': result_vector})
+    #profile_id = int(request.args.get('ProfileId'))
+    #city_id = int(request.args.get('CityId')) if request.args.get('CityId') is not None else None
+    #agent_service = Service()
+    #result_vector = agent_service.predict_trip_for_profile(profile_id, city_id)
+    #return jsonify({'Results': result_vector})
+	return jsonify({'Results': request.args.get('ProfileId')})
 
 
 if __name__ == '__main__':
