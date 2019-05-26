4351d148717b8e16f3ab2c0a186b8c380f0be2ff
AvrahamLa <avrahamla@gmail.com>
2019-05-27 01:01:53 +0300

    add back the function

--- app/flask_service.py
+++ app/flask_service.py
@@ -1,21 +1,21 @@
 from flask import Flask, request, jsonify
 from app.service import Service
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-	return 'OK!(:'
+    return 'OK!(:'
 
 
 @app.route('/aa')
 def index1():
-	aaa = Service()
-	#agent_service = Service()
-	return 'OK1111!'
+    aaa = Service()
+    # agent_service = Service()
+    return 'OK1111!'
 
 
-'''
 @app.route('/api/get_attractions_for_profile', methods=['GET'])
 def get_attractions_for_profile():
     profile_id = int(request.args.get('ProfileId'))
@@ -23,7 +23,7 @@ def get_attractions_for_profile():
     agent_service = Service()
     result_vector = agent_service.predict_trip_for_profile(profile_id, city_id)
     return jsonify({'Results': result_vector})
-'''
+
 
 if __name__ == '__main__':
     app.run()
