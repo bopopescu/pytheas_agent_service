a4a4cec3c6d14836944bd10cad9471c68094babf
AvrahamLa <avrahamla@gmail.com>
2019-05-27 09:40:41 +0300

    remove "results" wrap

--- app/flask_service.py
+++ app/flask_service.py
@@ -22,7 +22,7 @@ def get_attractions_for_profile():
     city_id = int(request.args.get('CityId')) if request.args.get('CityId') is not None else None
     agent_service = Service()
     result_vector = agent_service.predict_trip_for_profile(profile_id, city_id)
-    return jsonify({'Results': result_vector})
+    return jsonify(result_vector)
 
 
 if __name__ == '__main__':
