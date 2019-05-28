843fbd039afb0268b2144e8247a58fe69af2a0ef
AvrahamLa <avrahamla@etoro.com>
2019-05-28 18:32:12 +0300

    ignore config1

--- app/data_manager/config.py
+++ app/data_manager/config.py
@@ -2,7 +2,6 @@ mongoURL = "mongodb://test11:test11@ds215633.mlab.com:15633/pytheas"
 mongo_url = "mongodb://test11:test11@ds215633.mlab.com:15633/pytheas"
 mongo_db_name = 'pytheas'
 
-
 USERNAME = 'pytheas'
 PASSWORD = 'TravelMaker1'
 HOST = '35.240.98.26'
