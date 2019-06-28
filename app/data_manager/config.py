1b94507b6ec51a0383aa5f3df3e82502304fbf01
ShakedZrihen <shaked.zrihen@gmail.com>
2019-06-28 21:29:51 +0300

    Update db config

--- app/data_manager/config.py
+++ app/data_manager/config.py
@@ -1,11 +1,12 @@
+import os
+
 mongoURL = "mongodb://test11:test11@ds215633.mlab.com:15633/pytheas"
 mongo_url = "mongodb://test11:test11@ds215633.mlab.com:15633/pytheas"
 mongo_db_name = 'pytheas'
 
-USERNAME = 'pytheas'
-PASSWORD = 'TravelMaker1'
-HOST = '35.240.98.26'
+USERNAME = os.environ.get('DB_USERNAME')
+PASSWORD = os.environ.get('DB_PASSWORD')
+HOST = os.environ.get('DB_HOST')
 DATABASE_NAME = 'pytheas'
-CONNECTION_NAME = 'sigma-chemist-235920:europe-west1:pytheas-app'
 
 DB_URI = 'mysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}'
