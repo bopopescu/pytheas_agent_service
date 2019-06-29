f83ac30cec9a738e7a207c768aa257914b052438
AvrahamLa <avrahamla@etoro.com>
2019-06-29 16:16:27 +0300

    add config to gitignore

--- app/data_manager/config.py
+++ app/data_manager/config.py
@@ -2,10 +2,10 @@ mongoURL = "mongodb://test11:test11@ds215633.mlab.com:15633/pytheas"
 mongo_url = "mongodb://test11:test11@ds215633.mlab.com:15633/pytheas"
 mongo_db_name = 'pytheas'
 
-USERNAME = 'pytheas'
-PASSWORD = 'TravelMaker1'
-HOST = '35.240.98.26'
+USERNAME = 'root'
+PASSWORD = 'TravelMaker1!'
+HOST = 'pytheas-db.cofwkbzadfp1.eu-west-3.rds.amazonaws.com'
 DATABASE_NAME = 'pytheas'
-CONNECTION_NAME = 'sigma-chemist-235920:europe-west1:pytheas-app'
+CONNECTION_NAME = 'Pytheas-AWS'
 
 DB_URI = 'mysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}'
